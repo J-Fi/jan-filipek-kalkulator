7023603f0812ad2638bd1bdf6888840f797d8e19
J-Fi <y.neczek@gmail.com>
2019-04-27 00:46:32 +0200

    update no. 1

--- src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
@@ -7,13 +7,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KodillaCourseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KodillaCourseApplication.class, args);
+        Calculator calc = new Calculator(4,3);
+        double sum = calc.sum();
+        double substr = calc.substract();
+
+        System.out.println("Suma = " + sum + ", różnica = " + substr);
     }
-
-
-
 }
 
 class Calculator {
+
+    private double a;
+    private double b;
+
+    public Calculator (double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double sum() {
+        return a + b;
+    }
+
+    public double substract() {
+        return a - b;
+    }
 
 }
